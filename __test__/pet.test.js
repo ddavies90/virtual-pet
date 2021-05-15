58fdfd23f93fb57df09ef04c1439077827b23b94
David Davies <d.davies90@hotmail.co.uk>
2021-05-15 20:34:29 +0100

    Added hunger and fitness properties to Pet constructor. growUp() now decreases fitness and increases hunger. Added walk function which increases fitness. Maximum fitness will not exceed 10

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -1,4 +1,4 @@
-const Pet = require("../src/pet");
+const {Pet, MAXIMUM_FITNESS} = require("../src/pet");
 
 let pet;
 
@@ -12,12 +12,27 @@ describe("pet", () => {
         expect(pet).toBeInstanceOf(Object);
     });
 
-    it("Has an initial age of 0", () => {
+    //Can I check initial age, hunger and fitness in one test? Expect can only take one argument but it can take multiple expects ?is this the best way?
+    it("Has an initial age of 0, hunger of 0 and fitness of 10", () => {
         expect(pet.age).toEqual(0);
+        expect(pet.hunger).toEqual(0);
+        expect(pet.fitness).toEqual(10);
     });
 
-    it("Grows up by 1 when function run", () => {
+    it("When growUp is run: age increases by 1, hunger increases by 5 and fitness decreases by 3", () => {
         pet.growUp();
         expect(pet.age).toEqual(1);
-    })
+        expect(pet.hunger).toEqual(5);
+        expect(pet.fitness).toEqual(7);
+    });
+
+    it("When walk function is called, fitness is increased by 4 to a maximum of 10", () => {
+        pet.fitness = 5
+        pet.walk();
+        expect(pet.fitness).toEqual(9);
+
+        pet.walk();
+        expect(pet.fitness).toEqual(MAXIMUM_FITNESS);
+
+    });
 });  
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,23 @@
+const MAXIMUM_FITNESS = 10;
 
 function Pet (name) {
     this.name = name
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
 };
 Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+};
+Pet.prototype.walk = function () {
+    if (this.fitness + 4 > MAXIMUM_FITNESS) {
+        this.fitness = 10;
+    }
+    else {
+        this.fitness += 4;
+    };
 };
 
-module.exports = Pet; 
+module.exports = {Pet, MAXIMUM_FITNESS}; 
