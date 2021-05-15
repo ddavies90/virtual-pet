f7d2a0bed24817f5334a8a2a7632f35ad8c793b1
David Davies <d.davies90@hotmail.co.uk>
2021-05-15 23:51:39 +0100

    Added guard if statements to growUp, walk and feed to throw exception. Added status message to checkUp to account for pet not being alive. Refactored to add functions to protoype object rather than using dot notation to add them

--- src/pet.js
+++ src/pet.js
@@ -2,6 +2,12 @@ const MAXIMUM_FITNESS = 10;
 const MINIMUM_HUNGER = 0;
 const HUNGRY = 5;
 const UNFIT = 3;
+//Why does this break my code and throw an exception even when the if statement is false?
+// const guardFunc = function () {
+//     if (!this.isAlive) {
+//         throw 'Your pet is no longer alive :('
+//     };
+// };
 
 function Pet (name) {
     this.name = name
@@ -9,36 +15,55 @@ function Pet (name) {
     this.hunger = MINIMUM_HUNGER;
     this.fitness = MAXIMUM_FITNESS;
 };
-Pet.prototype.growUp = function() {
-    this.age += 1;
-    this.hunger += 5;
-    this.fitness -= 3;
-};
-Pet.prototype.walk = function () {
-    if (this.fitness + 4 > MAXIMUM_FITNESS) {
-        this.fitness = 10;
+
+Pet.prototype = {
+    get isAlive() {
+        return this.age < 30 && this.hunger < 10 && this.fitness > 0;
+    },
+    growUp() {
+        // guardFunc();
+        if (!this.isAlive) {
+            throw 'Your pet is no longer alive :('
+        };
+        this.age += 1;
+        this.hunger += 5;
+        this.fitness -= 3;
+    },
+    walk() {
+        if (!this.isAlive) {
+            throw 'Your pet is no longer alive :('
+        };
+        if (this.fitness + 4 > MAXIMUM_FITNESS) {
+            this.fitness = 10;
+        }
+        else {
+            this.fitness += 4;
+        };
+    },
+    feed() {
+        if (!this.isAlive) {
+            throw 'Your pet is no longer alive :('
+        };
+        if (this.hunger - 3 < MINIMUM_HUNGER) {
+            this.hunger = 0;
+        } else {
+            this.hunger -= 3;
+        };
+    },
+    checkUp() {
+        if (!this.isAlive) {
+            return "Your pet is no longer alive :(";
+        } else if (this.hunger >= HUNGRY && this.fitness <= UNFIT) {
+            return "I am hungry AND I need a walk"
+        } else if (this.hunger >= HUNGRY) {
+            return "I am hungry";
+        } else if (this.fitness <= UNFIT) {
+            return "I need a walk";
+        } else {
+            return "I feel great!";
+        };
     }
-    else {
-        this.fitness += 4;
-    };
 };
-Pet.prototype.feed = function() {
-    if (this.hunger - 3 < MINIMUM_HUNGER) {
-        this.hunger = 0;
-    } else {
-        this.hunger -= 3;
-    };
-};
-Pet.prototype.checkUp = function() {
-    if (this.hunger >= HUNGRY && this.fitness <= UNFIT) {
-        return "I am hungry AND I need a walk"
-    } else if (this.hunger >= HUNGRY) {
-        return "I am hungry";
-    } else if (this.fitness <= UNFIT) {
-        return "I need a walk";
-    } else {
-        return "I feel great!";
-    };
-};
+
 
 module.exports = {Pet, MAXIMUM_FITNESS, MINIMUM_HUNGER, HUNGRY, UNFIT}; 
